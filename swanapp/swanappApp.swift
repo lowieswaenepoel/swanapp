000cc0867fad61f5441c9fa33102c33fb9fae3ae
lowieswaenepoel <swaelowie@gmail.com>
2023-04-20 22:14:52 +0200

    Initial Commit

---
+++ swanapp/swanappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swanappApp.swift
+//  swanapp
+//
+//  Created by LOWIE SWAENEPOEL on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct swanappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
